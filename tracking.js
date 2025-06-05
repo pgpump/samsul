93dc676c80b524888ca58199a8d4e45d57934b6b
pgpump <pg079898@gmail.com>
2025-06-05 14:54:07 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,16 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4861131&101"/>');
+
+///// Statcounter /////
+var sc_project=13000036; 
+var sc_invisible=1; 
+var sc_security="5f73f73d";
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13000036/0/5f73f73d/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
